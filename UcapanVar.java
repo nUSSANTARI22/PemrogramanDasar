af61456c7a15032a7cbb48a2700a955b2f6ed04c
nUSSANTARI22 <mutiaranusantari5@gmail.com>
2026-09-20 01:48:09 +0800

    Update UcapanVar.java

--- UcapanVar.java
+++ UcapanVar.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package tipedata;
-
-/**
- *
- * @author nussantari
- */
 public class UcapanVar {
 
     public static void main(String[] args) {
