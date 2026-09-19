526e230a742f1f5e8d4f80903d2768db457fc07c
nUSSANTARI22 <mutiaranusantari5@gmail.com>
2026-09-20 00:43:43 +0800

    first commit

---
+++ src/tipedata/UcapanVar.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author nussantari
+ */
+public class UcapanVar {
+
+    public static void main(String[] args) {
+        String ucapan1, ucapan2, ucapan3;
+        ucapan1 = "Halo Indonesia";
+        ucapan2 = "Selamat Pagi Indonesia";
+        ucapan3 = "Jaya Indonesia";
+        
+        int jumlahKembalian = 3000;
+
+        System.out.println(ucapan1);
+        System.out.println(ucapan2);
+        System.out.println(ucapan3);
+        System.out.println(jumlahKembalian);
+        
+        int total = jumlahKembalian + 5000;
+        System.out.println(total);
+      
+    }
+
+}
